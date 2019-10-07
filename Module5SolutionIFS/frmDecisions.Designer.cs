11450b88474024860a5e1fa95d70ba06a14ed940
Kevin Tran <kevin115533@yahoo.com>
2019-10-07 15:01:32 -0600

    Weekly lab

---
+++ Module5SolutionIFS/frmDecisions.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace Module5SolutionIFS
+{
+    partial class frmDecisions
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblNum1 = new System.Windows.Forms.Label();
+            this.lblNum2 = new System.Windows.Forms.Label();
+            this.txtNum1 = new System.Windows.Forms.TextBox();
+            this.txtNum2 = new System.Windows.Forms.TextBox();
+            this.lblChecknumber = new System.Windows.Forms.Label();
+            this.txtCheckNumber = new System.Windows.Forms.TextBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(775, 65);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter a number in each text box, click the button to show if the Is Between numbe" +
+    "r is between the other two numbers";
+            // 
+            // lblNum1
+            // 
+            this.lblNum1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNum1.Location = new System.Drawing.Point(141, 78);
+            this.lblNum1.Name = "lblNum1";
+            this.lblNum1.Size = new System.Drawing.Size(159, 23);
+            this.lblNum1.TabIndex = 1;
+            this.lblNum1.Text = "Number 1";
+            // 
+            // lblNum2
+            // 
+            this.lblNum2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNum2.Location = new System.Drawing.Point(482, 78);
+            this.lblNum2.Name = "lblNum2";
+            this.lblNum2.Size = new System.Drawing.Size(159, 23);
+            this.lblNum2.TabIndex = 2;
+            this.lblNum2.Text = "Number 2";
+            // 
+            // txtNum1
+            // 
+            this.txtNum1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNum1.Location = new System.Drawing.Point(145, 104);
+            this.txtNum1.Name = "txtNum1";
+            this.txtNum1.Size = new System.Drawing.Size(159, 26);
+            this.txtNum1.TabIndex = 3;
+            this.txtNum1.TabStop = false;
+            // 
+            // txtNum2
+            // 
+            this.txtNum2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNum2.Location = new System.Drawing.Point(482, 104);
+            this.txtNum2.Name = "txtNum2";
+            this.txtNum2.Size = new System.Drawing.Size(159, 26);
+            this.txtNum2.TabIndex = 4;
+            this.txtNum2.TabStop = false;
+            // 
+            // lblChecknumber
+            // 
+            this.lblChecknumber.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblChecknumber.Location = new System.Drawing.Point(329, 153);
+            this.lblChecknumber.Name = "lblChecknumber";
+            this.lblChecknumber.Size = new System.Drawing.Size(159, 23);
+            this.lblChecknumber.TabIndex = 5;
+            this.lblChecknumber.Text = "Check Number";
+            this.lblChecknumber.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // txtCheckNumber
+            // 
+            this.txtCheckNumber.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.txtCheckNumber.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCheckNumber.Location = new System.Drawing.Point(329, 180);
+            this.txtCheckNumber.Name = "txtCheckNumber";
+            this.txtCheckNumber.Size = new System.Drawing.Size(159, 26);
+            this.txtCheckNumber.TabIndex = 6;
+            this.txtCheckNumber.TabStop = false;
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.Red;
+            this.btnClear.ForeColor = System.Drawing.Color.White;
+            this.btnClear.Location = new System.Drawing.Point(229, 309);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 7;
+            this.btnClear.Text = "Clea&r";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.BtnClear_Click);
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.BackColor = System.Drawing.Color.Red;
+            this.btnCheck.ForeColor = System.Drawing.Color.White;
+            this.btnCheck.Location = new System.Drawing.Point(365, 309);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(75, 23);
+            this.btnCheck.TabIndex = 8;
+            this.btnCheck.Text = "&Check";
+            this.btnCheck.UseVisualStyleBackColor = false;
+            this.btnCheck.Click += new System.EventHandler(this.BtnCheck_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.Red;
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.ForeColor = System.Drawing.Color.White;
+            this.btnExit.Location = new System.Drawing.Point(486, 309);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 9;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.BtnExit_Click);
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.BackColor = System.Drawing.Color.Black;
+            this.lblMessage.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMessage.ForeColor = System.Drawing.Color.Yellow;
+            this.lblMessage.Location = new System.Drawing.Point(17, 390);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(771, 23);
+            this.lblMessage.TabIndex = 10;
+            // 
+            // frmDecisions
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.txtCheckNumber);
+            this.Controls.Add(this.lblChecknumber);
+            this.Controls.Add(this.txtNum2);
+            this.Controls.Add(this.txtNum1);
+            this.Controls.Add(this.lblNum2);
+            this.Controls.Add(this.lblNum1);
+            this.Controls.Add(this.label1);
+            this.Name = "frmDecisions";
+            this.Text = "Decisions";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblNum1;
+        private System.Windows.Forms.Label lblNum2;
+        private System.Windows.Forms.TextBox txtNum1;
+        private System.Windows.Forms.TextBox txtNum2;
+        private System.Windows.Forms.Label lblChecknumber;
+        private System.Windows.Forms.TextBox txtCheckNumber;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblMessage;
+    }
+}
+
---
+++ Module5SolutionIFS/frmDecisions.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/*
+ * Author: Kevin Tran
+ * ITD 1252
+ * Fall 2019
+ * Due: 10/8/2019
+ * */
+
+namespace Module5SolutionIFS
+{
+    public partial class frmDecisions : Form
+    {
+        public frmDecisions()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnClear_Click(object sender, EventArgs e)
+        {
+            txtCheckNumber.Text = "";
+            txtNum1.Text = "";
+            txtNum2.Text = "";
+            lblMessage.Text = "";
+        }
+
+        private void BtnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void BtnCheck_Click(object sender, EventArgs e)
+        {
+            //Convert text in text boxes to int variables
+            int num1 = int.Parse(txtNum1.Text);
+            int num2 = int.Parse(txtNum2.Text);
+            int checkNum = int.Parse(txtCheckNumber.Text);
+            
+            if (checkNum == num1 || checkNum == num2)// This statement will check if the checked number is equal to any of the range numbers
+            {
+                if(checkNum == num1) // This nested statement will now decide which message to display 
+                {
+                    lblMessage.Text = "The checked number is equal to the low end number";
+                }
+                else if(checkNum == num2)
+                {
+                    lblMessage.Text = "The checked number is equal to the high end number";
+                }
+            }
+            else if (checkNum < num1) // Checks to see if the number is lower than the range
+            {
+                lblMessage.Text = "The checked number is smaller than the range";
+            }
+            else if (checkNum > num2) // Checks to see if the number is higher than the range
+            {
+                lblMessage.Text = "The checked number is larger than the range";
+            }
+            else // Checks for the last possible outcome
+            {
+                lblMessage.Text = "The checked number is within range";
+            }
+        }
+    }
+}
